90d4442da3a9c0fefd8934d4a250910273f361cd
Acacia B Ssembajjwe <acaciabengo@gmail.com>
2025-04-13 13:53:48 +0300

    wip unassin when assignee repliers

--- lib/activity_assigner.rb
+++ lib/activity_assigner.rb
@@ -1,48 +1,17 @@
 module ActivityAssigner
   SUPPORT_CATEGORIES = [67, 77, 85, 87, 88, 89, 102, 106, 4].freeze
+  ASSIGNMENT_GROUP = [86, 41]
+
   def self.process_post(post)
-    # Rails.logger.info("****** Processing Post ******")
-    # Rails.logger.info("****** First Post #{post.is_first_post?} ******")
-    # Rails.logger.info(
-    #   "****** In Cat #{post.topic.category_id.in?(SUPPORT_CATEGORIES)} ******"
-    # )
-    # Rails.logger.info(
-    #   "****** Assignment present #{post.topic.assignment.present?} ******"
-    # )
-    # Rails.logger.info(
-    #   "****** same user #{post.topic.assignment.assigned_to_id == post.user_id} ******"
-    # )
-    # topic = post.topic
-    # Rails.logger.info(
-    #   "========> Topic #{topic.id} & Category #{topic.category_id} ========="
-    # )
     if post.is_first_post? && post.topic.category_id.in?(SUPPORT_CATEGORIES)
-      # Rails.logger.info("========> First post #{post.id} =========")
-      # find two support users and assign the topic to the
-      #  query the database for the two users
-      assign_swat_user(post.topic) if SiteSetting.assign_enabled?
+      assign_support_user(post.topic) if SiteSetting.assign_enabled?
     end
 
-    # if !post.is_first_post? && topic.category_id.in?(SUPPORT_CATEGORIES)
-    #   # reload the topic to get the latest data
-    #   post.topic.reload
-
-    #   # find all users assigned to the topic]
-    #   if post.topic.assignment.present? &&
-    #        post.topic.assignment.assigned_to_id == post.user_id
-    #     # Rails.logger.info("========> UnAssigngin Post #{post.id} =========")
-    #     system_user = User.find_by(username: "system")
-    #     target = Topic.find(post.topic_id)
-    #     assigner = Assigner.new(target, system_user)
-    #     assigner.unassign
-
-    #     # Rails.logger.info(
-    #     #   "========> Unassigned Topic #{topic.id} Response: #{assigner} ========="
-    #     # )
-
-    #     assign_swat_user(topic)
-    #   end
-    # end
+    # if post is a reply from assigned user, unassign the topic and re-assign
+    # to another user
+    if !post.is_first_post? && post.topic.category_id.in?(SUPPORT_CATEGORIES)
+      if Assignment
+    end
   end
 
   def self.process_topic_tag(topic_tag)
@@ -60,7 +29,7 @@ module ActivityAssigner
     end
   end
 
-  def self.assign_swat_user(topic)
+  def self.assign_support_user(topic)
     system_user = User.find_by(username: "system")
     # assign to another user
     sql =
@@ -75,18 +44,19 @@ module ActivityAssigner
 
             SELECT user_id
             FROM group_users
-            WHERE group_id IN (54,41) AND
+            WHERE group_id IN (?) AND
             user_id NOT IN (SELECT assigned_to_id FROM assigned_users)
             ORDER BY RANDOM()
             LIMIT 1
           "
-    results = ActiveRecord::Base.connection.execute(sql)
+    # results = ActiveRecord::Base.connection.execute(sql)
+    results =
+      ActiveRecord::Base.connection.execute(
+        ActiveRecord::Base.send(:sanitize_sql_array, [sql, group_ids])
+      )
     return unless results.count > 0
     user = results&.first
     user = User.find(user["user_id"])
-    # Rails.logger.info(
-    #   "========> Assigning Topic #{topic} from SWAT Assigner ========="
-    # )
 
     assign = Assigner.new(topic, system_user).assign(user)
     unless assign[:success]
