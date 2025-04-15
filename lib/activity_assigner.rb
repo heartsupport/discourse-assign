220d29888ed7528f0fb6abffa8c7a0eeed5e7ec1
Acacia B Ssembajjwe <acaciabengo@gmail.com>
2025-04-15 12:59:16 +0300

    finished testing assign plugin

--- lib/activity_assigner.rb
+++ lib/activity_assigner.rb
@@ -1,6 +1,6 @@
 module ActivityAssigner
   SUPPORT_CATEGORIES = [67, 77, 85, 87, 88, 89, 102, 106, 4].freeze
-  ASSIGNMENT_GROUP = [86, 41]
+  ASSIGNMENT_GROUP_IDS = [86]
 
   def self.process_post(post)
     if post.is_first_post? && post.topic.category_id.in?(SUPPORT_CATEGORIES)
@@ -8,15 +8,15 @@ module ActivityAssigner
     end
 
     if !post.is_first_post? && post.topic.category_id.in?(SUPPORT_CATEGORIES)
+      assignment = post.topic.assignment
       # if post user is the assigned to, then unassign and assign another person
-      if post.user_id == post.topic.assigned_to_id
-        # unassign the topic
+      if assignment && post.user_id == assignment.assigned_to_id
         system_user = User.find_by(username: "system")
-        assigner = Assigner.new(post.topic, system_user)
-        assigner.unassign
+
+        Assigner.new(post.topic, system_user).unassign
 
         # assign to another user
-        assign_support_user(post.topic)
+        assign_support_user(post.topic) if SiteSetting.assign_enabled?
       end
     end
   end
@@ -31,8 +31,7 @@ module ActivityAssigner
     # find all users assigned to the topic]
     if topic_tag.topic.assignment.present?
       system_user = User.find_by(username: "system")
-      assigner = Assigner.new(topic_tag.topic, system_user)
-      assigner.unassign
+      Assigner.new(topic_tag.topic, system_user).unassign
     end
   end
 
@@ -41,26 +40,30 @@ module ActivityAssigner
     # assign to another user
     sql =
       "
-            WITH assigned_users AS (
-              SELECT assigned_to_id, COUNT(assigned_to_id) as assigned_count
-              FROM assignments
-              WHERE created_at >= CURRENT_DATE - INTERVAL '7 Days'
-              GROUP BY assigned_to_id
-              HAVING COUNT(assigned_to_id) >= 4
-            )
+              WITH assigned_users AS (
+                SELECT assigned_to_id, COUNT(assigned_to_id) as assigned_count
+                FROM assignments
+                WHERE created_at >= CURRENT_DATE - INTERVAL '7 Days'
+                GROUP BY assigned_to_id
+                HAVING COUNT(assigned_to_id) >= 4
+              )
 
-            SELECT user_id
-            FROM group_users
-            WHERE group_id IN (?) AND
-            user_id NOT IN (SELECT assigned_to_id FROM assigned_users)
-            ORDER BY RANDOM()
-            LIMIT 1
-          "
-    # results = ActiveRecord::Base.connection.execute(sql)
+              SELECT user_id
+              FROM group_users
+              WHERE group_id IN (?) AND
+              user_id NOT IN (SELECT assigned_to_id FROM assigned_users)
+              ORDER BY RANDOM()
+              LIMIT 1
+            "
+
     results =
       ActiveRecord::Base.connection.execute(
-        ActiveRecord::Base.send(:sanitize_sql_array, [sql, group_ids])
+        ActiveRecord::Base.send(
+          :sanitize_sql_array,
+          [sql, ASSIGNMENT_GROUP_IDS]
+        )
       )
+
     return unless results.count > 0
     user = results&.first
     user = User.find(user["user_id"])
--- spec/lib/activity_assigner_spec.rb
+++ spec/lib/activity_assigner_spec.rb
@@ -6,31 +6,89 @@ RSpec.describe ActivityAssigner do
     SiteSetting.enable_assign_status = true
   end
 
+  before(:each) do
+    stub_request(
+      :post,
+      "https://porter.heartsupport.com/webhooks/supplier"
+    ).to_return(status: 200, body: "", headers: {})
+    stub_request(
+      :post,
+      "https://porter.heartsupport.com/twilio/discourse_webhook"
+    ).to_return(status: 200, body: "", headers: {})
+    stub_request(
+      :post,
+      "https://porter.heartsupport.com/webhooks/supplier"
+    ).to_return(status: 200, body: "", headers: {})
+    stub_request(
+      :get,
+      %r{https\://porter.heartsupport.com/api/sentiment}
+    ).to_return(status: 200, body: "", headers: {})
+    stub_request(
+      :post,
+      "https://porter.heartsupport.com/webhooks/topic_tags"
+    ).to_return(status: 200, body: "", headers: {})
+  end
+
   describe "assigning a user to a topic" do
-    let(:category) { Fabricate(:category) }
+    let(:group) { Fabricate(:group, id: 86) }
+    let(:category) { Fabricate(:category, id: 67) }
     let(:topic) { Fabricate(:topic, category: category) }
-    let(:post) { Fabricate(:post, topic: topic) }
     let(:user) { Fabricate(:user) }
+    let(:assigner) { instance_double(Assigner) }
+    let(:system_user) { User.find_by(username: "system") }
+    let(:support_user) { Fabricate(:user) }
+    let!(:group_user) do
+      Fabricate(:group_user, group: group, user: support_user)
+    end
+    let(:post) { instance_double(Post) }
 
     before do
-      # SiteSetting.assign_enabled = true
-      # SiteSetting.enable_assign_status = true
-      # SiteSetting.assign_group_id = 1
-      # SiteSetting.assign_group_name = "Support"
-      # SiteSetting.assign_group_description = "Support group"
-      # SiteSetting.assign_group_color = "#000000"
-      # SiteSetting.assign_group_icon = "fa-user"
+      SiteSetting.assign_allowed_on_groups = "#{group.id}"
+      allow(post).to receive(:is_first_post?).and_return(true)
+      allow(User).to receive(:find_by).with(username: "system").and_return(
+        system_user
+      )
+      allow(post).to receive(:topic).and_return(topic)
+      allow(topic).to receive(:category_id).and_return(category.id)
+      allow(post).to receive(:is_first_post?).and_return(true)
     end
 
-    it "assigns a user to the topic" do
-      expect(ActivityAssigner).to receive(:assign_support_user).with(topic)
-      expect(DiscourseAssign::Assigner).to receive(:new).with(topic, user)
-      expect { ActivityAssigner.process_post(post) }.to change {
-        Assignment.count
-      }.by(1)
+    it "assigns a support user to the topic" do
+      # create a post
+      expect {
+        post = Fabricate(:post, topic: topic, user: user)
+        post.save!
+      }.to change { Assignment.count }.by(1)
+
+      expect(post.topic.reload&.assignment&.assigned_to).to eq(support_user)
     end
 
     it "unassigns a user from the topic when they reply" do
+      post = Fabricate(:post, topic: topic, user: user)
+      post.save!
+
+      expect(post.topic.reload&.assignment&.assigned_to).to eq(support_user)
+
+      # create a second group user
+      user = Fabricate(:user)
+      group_user = Fabricate(:group_user, group: group, user: user)
+      group_user.save!
+
+      reply = # post a reply
+        Fabricate(:post, topic: topic, user: support_user)
+      reply.save!
+
+      # expect the user to be unassigned
+      expect(reply.topic.reload&.assignment&.assigned_to).to eq(user)
+    end
+
+    it 'removes assignment when the topic is tagged with "Supported"' do
+      post = Fabricate(:post, topic: topic, user: user)
+      post.save!
+      post.topic.tags << Tag.find_or_create_by(name: "Supported")
+      post.topic.save!
+
+      expect(post.topic.reload.assignment).to be_nil
     end
   end
 end
